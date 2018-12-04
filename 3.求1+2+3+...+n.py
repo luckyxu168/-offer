47b41adfb5a6ce0fb3616edd21cd8a5056d59d97
luckyxu168 <45558321+luckyxu168@users.noreply.github.com>
2018-12-04 16:34:17 +0800

    Add files via upload

---
+++ 3.求1+2+3+...+n.py
@@ -0,0 +1,12 @@
+#求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+# 1.使用递归
+def Sum_Solution( n):
+    if n == 1:
+        return 1
+    return Sum_Solution(n - 1) + n
+
+print(Sum_Solution(100))
+
+# 2.使用求和函数
+s = sum(list(range(1,101)))
